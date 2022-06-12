f48f8e7941a20574ca5d67436e192b6e5d64ca5f
shivam tyagi <shivamt.ug19.ec@nitp.ac.in>
2022-06-12 10:52:55 +0530

    update

---
+++ client/file.js
@@ -0,0 +1,27 @@
+// react format
+const renderForm=(
+    <div className="form-group"> 
+    <form>
+        <div className="input">
+            <label>Username</label>
+            <input type="text" name="uname"required/>
+
+        </div>
+        <div className="input">
+            <label>password</label>
+            <input type="password" name="pass"required/>
+
+        </div>
+        <div className="button-container">
+           
+            <input type="submit" name="uname"required/>
+
+        </div>
+
+
+    </form>
+    </div>
+
+
+
+);
